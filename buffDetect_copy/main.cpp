7af679b1baf7c6d85bf7b0bfd6c91415506f1ee9
fqjun <1767851382@qq.com>
2020-08-29 21:20:40 +0800

    宏定义模块开关

--- buffDetect_copy/main.cpp
+++ buffDetect_copy/main.cpp
@@ -9,20 +9,56 @@ int main(){
     cap.open("/home/jun/workplace/RM/buffDetect/data/camera_13.avi");
     Mat frame;
 
+    int common = 0;
+    int firing_times = 0;
+
     BuffDetector buff;
     while (true) {
         cap >> frame;
         if(frame.empty())
             break;
         resize(frame, frame, Size(640,480));
-        double t = getTickCount();
-        buff.buffDetect_Task(frame);
-        t = ((double)getTickCount() - t) / getTickFrequency();
-        //cout << "t:" << t << endl;
-//        double fps = 1.0 / t;
-//        cout << "fps:" << fps << endl;
+        double t1 = (double)getTickCount();//开始计时
+        common = buff.buffDetect_Task(frame);
+        double t2 = (double)getTickCount();//开始计时
+        double t = (t2 - t1) / getTickFrequency();//结束计时
+#ifdef FPS
+
+        cout << "t:" << t << endl;
+        int fps = int(1.0 / t);//转换为帧率
+        cout << "fps:" << fps << endl;
+        Point Fps = Point(10,20);
+        char text[50];
+        sprintf(text,"FPS:%d",fps);
+        putText(frame,text,Fps,FONT_HERSHEY_PLAIN,1,Scalar(100,200,0),1,8,false);
         cout<<"-----------------------------"<<endl;
-        char c = waitKey(20);
+
+#endif
+#ifdef auto_control_text
+        //开火 5 1 3
+        //跟随 3 1
+        //不跟随 2 0
+        //复位 5 6
+
+        char times[50];
+        sprintf(times,"Firing times:%d",firing_times);
+        Point F_times = Point(frame.cols-200,20);
+        putText(frame,times,F_times,FONT_HERSHEY_PLAIN,1,Scalar(100,200,100),1,8,false);
+
+        Point autocommon = Point(frame.cols-100,frame.rows-20);
+        if(common == 1){
+            putText(frame,"FIRE",autocommon,FONT_HERSHEY_PLAIN,1,Scalar(100,200,100),1,8,false);
+            firing_times ++;
+        }else if(common == 2){
+            putText(frame,"RESET",autocommon,FONT_HERSHEY_PLAIN,1,Scalar(100,200,100),1,8,false);
+        }else if(common == 3){
+            putText(frame,"FOLLOW",autocommon,FONT_HERSHEY_PLAIN,1,Scalar(100,200,100),1,8,false);
+        }else if(common == 4){
+            putText(frame,"UNFOLLOW",autocommon,FONT_HERSHEY_PLAIN,1,Scalar(100,200,100),1,8,false);
+        }
+#endif
+        imshow("img", frame);
+        char c = waitKey(0);//20
         if(c == 27)
             break;
 
