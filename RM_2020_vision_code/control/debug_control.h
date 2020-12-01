bcbcc179cc59e6fc4c57a67f055f0055f750b84b
fqjun <1767851382@qq.com>
2020-12-01 21:52:26 +0800

    V2.2 修改宏定义开关

--- RM_2020_vision_code/control/debug_control.h
+++ RM_2020_vision_code/control/debug_control.h
@@ -1,7 +1,7 @@
 #ifndef DEBUG_CONTROL_H
 #define DEBUG_CONTROL_H
 
-#define CAMERA_OR_VEDIO 0
+#define CAMERA_OR_VEDIO 1
 /**
  * @brief 使用相机或者视频
  * @param: 0      相机
@@ -65,7 +65,7 @@
 */
 /*---------------------------------------------------*/
 
-#define MY_COLOR 0
+#define MY_COLOR 1
 /**
   @brief: 选择己方阵营
   @param: 0     不限制颜色
@@ -75,7 +75,7 @@
   @note: 强制颜色模式
 */
 
-#define MY_MODE 5
+#define MY_MODE 2
 /**
  * @brief :选择己方模式
  * @param:0     默认模式
@@ -160,7 +160,7 @@
   @param: 0     不打印
   @param: 1     打印
 */
-#define IS_PARAM_ADJUSTMENT 0
+#define IS_PARAM_ADJUSTMENT 1
 /**
   @brief: 是否进入调参模式
   @param: 0     否
