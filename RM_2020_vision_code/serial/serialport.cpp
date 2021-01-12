36252752d4bc7aa8cdd3dda4a6acb5e7bba6f501
GCUROBOT-Visual-Group <GCUROBOT_WOLF@163.com>
2021-01-13 00:19:27 +0800

    修改警告

--- RM_2020_vision_code/serial/serialport.cpp
+++ RM_2020_vision_code/serial/serialport.cpp
@@ -172,11 +172,11 @@ void SerialPort::RMserialWrite(int _yaw,int16_t yaw,int _pitch,int16_t pitch,int
     <<"  "<<static_cast<int>(g_write_buf[6])<<"  "<<float(_pitch_reduction)/1000 \
     <<"  "<<float(_depth_reduction)<<"  "<<g_write_buf[12]<<endl;
     #else
-    cout<<"g_write_buf=  "<<g_write_buf[0] \ 
-    <<"  "<<static_cast<int>(g_write_buf[1]) \ 
-    <<"  "<<static_cast<int>(g_write_buf[2]) \ 
-    <<"  "<<static_cast<int>(g_write_buf[3])<<"  "<<float(_yaw_reduction) \ 
-    <<"  "<<static_cast<int>(g_write_buf[6])<<"  "<<float(_pitch_reduction) \ 
+    cout<<"g_write_buf=  "<<g_write_buf[0] \
+    <<"  "<<static_cast<int>(g_write_buf[1]) \
+    <<"  "<<static_cast<int>(g_write_buf[2]) \
+    <<"  "<<static_cast<int>(g_write_buf[3])<<"  "<<float(_yaw_reduction) \
+    <<"  "<<static_cast<int>(g_write_buf[6])<<"  "<<float(_pitch_reduction) \
     <<"  "<<float(_depth_reduction)<<"  "<<g_write_buf[12]<<endl;
     #endif
     _yaw_reduction = 0x0000;
